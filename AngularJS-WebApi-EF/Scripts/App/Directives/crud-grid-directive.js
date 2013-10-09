7c2d7d6f2174dbf201ed015e1f38cccb407cd1fc
Jon Gallant <jon@jongallant.fake>
2013-10-09 16:17:59 -0700

    Added support for dynamic columns.

--- AngularJS-WebApi-EF/Scripts/App/Directives/crud-grid-directive.js
+++ AngularJS-WebApi-EF/Scripts/App/Directives/crud-grid-directive.js
@@ -5,62 +5,64 @@
         scope: true,
         templateUrl: '/Scripts/App/Directives/Templates/crud-grid-directive-template.html',
         controller: ['$scope', '$element', '$attrs', 'crudGridDataFactory', 'notificationFactory',
-            function ($scope, $element, $attrs, crudGridDataFactory, notificationFactory) {
+            function($scope, $element, $attrs, crudGridDataFactory, notificationFactory) {
                 $scope.objects = [];
+                $scope.object = {};
+                $scope.columns = angular.fromJson($attrs.columns);
                 $scope.addMode = false;
                 $scope.orderBy = { field: 'Name', asc: true };
                 $scope.loading = true;
 
-                $scope.toggleAddMode = function () {
+                $scope.toggleAddMode = function() {
                     $scope.addMode = !$scope.addMode;
                 };
 
-                $scope.toggleEditMode = function (object) {
+                $scope.toggleEditMode = function(object) {
                     object.editMode = !object.editMode;
                 };
 
-                var successCallback = function (e, cb) {
+                var successCallback = function(e, cb) {
                     notificationFactory.success();
                     $scope.getData(cb);
                 };
 
-                var successPostCallback = function (e) {
-                    successCallback(e, function () {
+                var successPostCallback = function(e) {
+                    successCallback(e, function() {
                         $scope.toggleAddMode();
                         $scope.object = {};
                     });
                 };
 
-                var errorCallback = function (e) {
+                var errorCallback = function(e) {
                     notificationFactory.error(e.data.ExceptionMessage);
                 };
 
-                $scope.addObject = function () {
-                    crudGridDataFactory($attrs.tableName).save($scope.object, successPostCallback, errorCallback);
+                $scope.addObject = function() {
+                    crudGridDataFactory($attrs.table).save($scope.object, successPostCallback, errorCallback);
                 };
 
-                $scope.deleteObject = function (object) {
-                    crudGridDataFactory($attrs.tableName).delete({ id: object.Id }, successCallback, errorCallback);
+                $scope.deleteObject = function(object) {
+                    crudGridDataFactory($attrs.table).delete({ id: object.Id }, successCallback, errorCallback);
                 };
 
-                $scope.updateObject = function (object) {
-                    crudGridDataFactory($attrs.tableName).update({ id: object.Id }, object, successCallback, errorCallback);
+                $scope.updateObject = function(object) {
+                    crudGridDataFactory($attrs.table).update({ id: object.Id }, object, successCallback, errorCallback);
                 };
 
-                $scope.getData = function (cb) {
+                $scope.getData = function(cb) {
 
-                    crudGridDataFactory($attrs.tableName).query(function (data) {
+                    crudGridDataFactory($attrs.table).query(function(data) {
                         $scope.objects = data;
                         if (cb) cb();
                     });
                 };
 
-                $scope.setOrderBy = function (field) {
+                $scope.setOrderBy = function(field) {
                     var asc = $scope.orderBy.field === field ? !$scope.orderBy.asc : true;
                     $scope.orderBy = { field: field, asc: asc };
-                }
+                };
 
-                $scope.getData(function () { $scope.loading = false; });
+                $scope.getData(function() { $scope.loading = false; });
             }]
-    }
+    };
 });
